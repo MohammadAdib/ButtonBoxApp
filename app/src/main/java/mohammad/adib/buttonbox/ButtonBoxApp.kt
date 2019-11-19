7c1f04b8302722d4a093d6a8e01b47df7a48f08a
Mohammad Adib <Mohammad.Adib@samsclub.com>
2019-11-19 00:53:26 -0800

    connectivity perfection

--- app/src/main/java/mohammad/adib/buttonbox/ButtonBoxApp.kt
+++ app/src/main/java/mohammad/adib/buttonbox/ButtonBoxApp.kt
@@ -3,6 +3,7 @@ package mohammad.adib.buttonbox
 import android.app.Application
 import android.content.SharedPreferences
 import android.preference.PreferenceManager
+import androidx.lifecycle.MutableLiveData
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
@@ -10,9 +11,13 @@ import java.net.InetAddress
 class ButtonBoxApp : Application() {
 
     private lateinit var preferences: SharedPreferences
+    private val broadcast = InetAddress.getByName("255.255.255.255")
     private var ip: InetAddress? = null
+    var serverNameLiveData = MutableLiveData<String>()
+    private var searching = false
+    var connected = false
     private val port = 18250
-    private var serverName: String? = null
+    private val socket = DatagramSocket(port)
 
     override fun onCreate() {
         super.onCreate()
@@ -24,6 +29,14 @@ class ButtonBoxApp : Application() {
     companion object {
         lateinit var instance: ButtonBoxApp
             private set
+    }
+
+    private fun setServer(address: InetAddress?) {
+        ip = address
+    }
+
+    private fun setServerName(name: String?) {
+        serverNameLiveData.postValue(name)
     }
 
     private fun setupDefaultMapping() {
@@ -41,31 +54,61 @@ class ButtonBoxApp : Application() {
             .apply()
     }
 
-    fun setServer(address: InetAddress) {
-        ip = address
-    }
-
-    fun setServerName(name: String) {
-        serverName = name
-    }
-
-    fun isConnected(): Boolean {
-        return ip != null
-    }
-
     private fun getKeyCode(tag: String): Int {
         return preferences.getInt(tag, 0)
     }
 
     fun dispatchButtonPress(tag: String) {
         val code = getKeyCode(tag).toString()
-        ip?.let {
-            Thread {
+        Thread {
+            val socket = DatagramSocket()
+            val data = code.toByteArray()
+            val packet = DatagramPacket(data, data.size, ip ?: broadcast, port)
+            socket.send(packet)
+        }.start()
+    }
+
+    fun startSearch() {
+        Thread {
+            var tries = 0
+            connected = false
+            while (searching && tries < 5) {
                 val socket = DatagramSocket()
-                val data = code.toByteArray()
-                val packet = DatagramPacket(data, data.size, ip, port)
+                val data = "buttonbox-ack".toByteArray()
+                val packet = DatagramPacket(data, data.size, broadcast, port)
                 socket.send(packet)
-            }.start()
-        }
+                Thread.sleep(500)
+                tries++
+            }
+            searching = false
+            if (!connected) {
+                setServer(null)
+                setServerName(null)
+            }
+        }.start()
+        searching = true
+        receiveResponses()
+    }
+
+    private fun receiveResponses() {
+        Thread {
+            while (searching) {
+                val buffer = ByteArray(2048)
+                val packet = DatagramPacket(buffer, buffer.size)
+                socket.receive(packet)
+                val message = String(buffer).toCharArray()
+                    .filter {
+                        it.isLetterOrDigit() ||
+                                it == '-' || it == '_' || it == ' ' || it == '|'
+                    }
+                    .joinToString(separator = "")
+                if (!message.startsWith("buttonbox-ack")) {
+                    setServer(packet.address)
+                    setServerName(message)
+                    searching = false
+                    connected = true
+                }
+            }
+        }.start()
     }
 }
--- app/src/main/java/mohammad/adib/buttonbox/MainActivity.kt
+++ app/src/main/java/mohammad/adib/buttonbox/MainActivity.kt
@@ -4,26 +4,53 @@ import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.os.Vibrator
+import android.text.SpannableString
+import android.text.util.Linkify
 import android.view.View
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity(), Observer<String> {
+
+    private var serverDialog: AlertDialog? = null
     private var vibrator: Vibrator? = null
+    private var timer = Timer()
 
     @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
-        findServer()
+        ButtonBoxApp.instance.serverNameLiveData.observe(this, this)
+        timer.scheduleAtFixedRate(Heartbeat(), 0, 10000)
     }
 
-    private fun findServer() {
-        val dialog = ServerDialog()
-        dialog.isCancelable = false
-        dialog.show(supportFragmentManager, "server")
+    override fun onChanged(name: String?) {
+        serverName.text = if (name.isNullOrEmpty()) getString(R.string.not_connected) else name
+        serverName.setCompoundDrawablesWithIntrinsicBounds(
+            if (name.isNullOrEmpty()) R.drawable.red_dot else R.drawable.green_dot,
+            0,
+            0,
+            0
+        )
+        if (name.isNullOrEmpty()) {
+            if (serverDialog == null) {
+                val s = SpannableString(getString(R.string.download_server))
+                Linkify.addLinks(s, Linkify.ALL)
+                serverDialog = AlertDialog.Builder(this)
+                    .setTitle(getString(R.string.no_server_found))
+                    .setMessage(s)
+                    .create()
+            }
+            serverDialog?.let { if (!it.isShowing) it.show() }
+        } else {
+            serverDialog?.let { if (it.isShowing) it.dismiss() }
+        }
     }
 
     fun buttonPress(v: View) {
@@ -31,7 +58,26 @@ class MainActivity : AppCompatActivity() {
         vibrate()
     }
 
+    fun displayInfo(v: View) {
+        if (v.id == R.id.serverName) {
+            if (!ButtonBoxApp.instance.connected) serverDialog?.let { if (!it.isShowing) it.show() }
+        } else {
+            val s = SpannableString(getString(R.string.info))
+            Linkify.addLinks(s, Linkify.ALL)
+            AlertDialog.Builder(this)
+                .setTitle(R.string.app_name)
+                .setMessage(s)
+                .show()
+        }
+    }
+
     private fun vibrate() {
         vibrator?.vibrate(25)
+    }
+
+    private class Heartbeat : TimerTask() {
+        override fun run() {
+            ButtonBoxApp.instance.startSearch()
+        }
     }
 }
